6c9a9b186c05e6a249858858bd86f3f8bd1884f5
Nathan Jansen <info@nathanjansen.nl>
2023-08-14 14:54:30 +0200

    Fixed migration

--- database/migrations/2023_08_14_144214_add_user_to_blessings_table.php
+++ database/migrations/2023_08_14_144214_add_user_to_blessings_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
 
         Schema::table('blessings', function (Blueprint $table) {
             $table->after('id', function (Blueprint $table) {
-                $table->integer('user_id');
+                $table->integer('user_id')->unsigned();
             });
         });
     }

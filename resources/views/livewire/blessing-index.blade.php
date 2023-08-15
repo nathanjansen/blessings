a06d9273128cd0213be81bfa3c6dcdc202d9d1b8
Nathan Jansen <info@nathanjansen.nl>
2023-08-15 21:52:06 +0200

    wip

--- resources/views/livewire/blessing-index.blade.php
+++ resources/views/livewire/blessing-index.blade.php
@@ -25,13 +25,13 @@
             <x-daily-verse :day="$carbonDate" />
 
             <div>
-                <x-webpush::subscribe-button
+                <x-convey::subscribe-button
                     class="border p-2 border-black rounded text-black hover:shadow-md hover:bg-black hover:text-white transition transition-all active:shadow-none active:bg-purple-600 active:text-white"
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                     </svg>
-                </x-webpush::subscribe-button>
+                </x-convey::subscribe-button>
             </div>
         </div>
     </div>

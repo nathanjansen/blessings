f31e2dd60733d28f654f1ca45e4ba16b1060b548
Nathan Jansen <info@nathanjansen.nl>
2023-09-06 20:12:50 +0200

    Changed the email subjects

--- app/Notifications/Reminder.php
+++ app/Notifications/Reminder.php
@@ -37,17 +37,15 @@ class Reminder extends Notification
         $verse = Verse::today();
 
         $subjects = [
-            'Waar mag jij vandaag dankbaar voor zijn?',
-            'Koester je dagelijkse dankbaarheid?',
-            'Geeft dankbaar zijn je vreugde?',
-            'Schrijf je dagelijkse vreugde op?',
-            'Voedt dankbaarheid je positiviteit?',
-            'Vang je geluk door dankbaarheid?',
-            'Is dankbaar zijn als innerlijke straling?',
-            'Ben je elke dag voor iets dankbaar?',
-            'Is dankbaarheid jouw gewoonte?',
-            'Groei je door dankbaarheid?',
-            'Verrijkt dankbaarheid je leven?',
+            'Wat liet je vandaag glimlachen?',
+            'Wie beïnvloedde je dag positief?',
+            'Ervaarde je vandaag rust?',
+            'Welk gebaar maakte jouw dag?',
+            'Wat inspireerde je vandaag?',
+            'Jouw overwinning vandaag?',
+            'Beste herinnering vandaag?',
+            'Wat verraste je vandaag?',
+            'Herbeleef een moment van vandaag?',
         ];
 
         return (new MailMessage)
